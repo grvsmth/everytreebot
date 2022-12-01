6e6649b830f15aab297004b6bc71a3665af96560
Angus Grieve-Smith <angus@grieve-smith.com>
2022-12-01 05:11:55 +0000

    Separate out Mastodon functions and upload media first

--- everylot/mastodon.py
+++ everylot/mastodon.py
@@ -15,36 +15,35 @@ ATTACHMENT_ERROR = 'Impossible de joindre les fichiers en cours de traitement. R
 
 def mastodon_image(mastodon, title_image):
     mime_type = "image/png"
-    media_res = mastodon.media_post(title_image, mime_type)
+    media_res = mastodon.media_post(title_image.getvalue(), mime_type)
 
     if (media_res.get("type") != "image"):
+        LOG.error("Unable to post media! %s", media_res)
         return None
 
     return str(media_res.get("id", None))
 
 
-def send_toot(config, message, title_image):
-    media_id = None
-
-    mastodon = Mastodon(client_id = config['client_id'],
+def mastodon_instance(config):
+    return Mastodon(client_id = config['client_id'],
         client_secret = config['client_secret'],
         access_token = config['access_token'],
         api_base_url = config['base_uri'])
 
-    if title_image:
-        media_id = mastodon_image(mastodon, title_image)
-        if media_id:
-            LOG.info("media_id=" + media_id)
-            # Give the server a little time to process our pic
-            sleep(MEDIA_WAIT_TIME)
+
+def send_toot(mastodon, message, media_id):
+    if media_id:
+        LOG.info("media_id=" + media_id)
+        # Give the server a little time to process our pic
+        sleep(MEDIA_WAIT_TIME)
 
     try:
         status = mastodon.status_post(message, media_ids=[media_id])
     except MastodonAPIError as err:
-        LOG.error("Unable to post status!  Trying without media... %s",
-            err)
+        LOG.error("Unable to post status! %s", err)
 
         if err.args[3] == ATTACHMENT_ERROR:
+            LOG.error("Trying without media...")
             mastodon.status_post(message)
 
     if 'id' in status:
